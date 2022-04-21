3f08e69ca323b3379b3617c26a0cf2c4751036b3
Aleshko <aleshko.info@gmail.com>
2022-04-21 11:20:02 +0200

    added blink text

--- scripts.js
+++ scripts.js
@@ -60,11 +60,21 @@ const slider = function(id) {
       btnRight.addEventListener('click',right);  
     }
 
-
-
-    
-
 };
 
 slider('slider-head');
 slider('slider-head1');
+
+let btns = document.querySelectorAll("*[data-modal-btn]");
+
+for(let i = 0; i < btns.length;i++){
+  btns[i].addEventListener('click', function(){
+    let name = btns[i].getAttribute('data-modal-btn');
+    let modal = document.querySelector("[data-modal-window = '"+name+"']");
+    modal.style.display = 'block';
+    let close = modal.querySelector('.close_modal_window');
+    close.addEventListener('click', function(){
+      modal.style.display = "none";
+    });
+  });
+}
